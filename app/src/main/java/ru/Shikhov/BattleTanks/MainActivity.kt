2aa1618c0c45fae8fdbe5eaaca3e51a611752b1c
Radmir <radshi5@mail.ru>
2025-03-22 14:40:09 +0300

    2 presentation

--- app/src/main/java/ru/Shikhov/BattleTanks/MainActivity.kt
+++ app/src/main/java/ru/Shikhov/BattleTanks/MainActivity.kt
@@ -7,20 +7,49 @@ import android.view.KeyEvent.KEYCODE_DPAD_DOWN
 import android.view.KeyEvent.KEYCODE_DPAD_LEFT
 import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
 import android.view.KeyEvent.KEYCODE_DPAD_UP
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.FrameLayout
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
 import ru.Shikhov.BattleTanks.Direction.UP
 import ru.Shikhov.BattleTanks.Direction.DOWN
 import ru.Shikhov.BattleTanks.Direction.LEFT
 import ru.Shikhov.BattleTanks.Direction.RIGHT
 import ru.Shikhov.BattleTanks.databinding.ActivityMainBinding
 
+const val CELL_SIZE = 50
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val gridDrawer by lazy{
+        GridDrawer(this)
+    }
+
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title="Menu"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.menu_settings ->{
+                gridDrawer.drawGrid()
+                return true
+            }
+            else ->super.onOptionsItemSelected(item)
+        }
     }
 
 override fun onKeyDown(keyCode:Int,event: KeyEvent?):Boolean {
@@ -33,26 +62,39 @@ override fun onKeyDown(keyCode:Int,event: KeyEvent?):Boolean {
     return super.onKeyDown(keyCode, event)
 }
 
-private fun move(direction: Direction){
-    when(direction) {
-        UP -> {
-            binding.myTank.rotation = 0f
-            (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
+    private fun move(direction: Direction){
+        when(direction){
+
+
+            UP->{
+                binding.myTank.rotation = 0f
+                if(binding.myTank.marginTop>0){
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+                }
+            }
+
+            DOWN->{
+                if(binding.myTank.marginTop+ binding.myTank.height< binding.container.height/ CELL_SIZE* CELL_SIZE){
+                    binding.myTank.rotation = 180f
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+                }
+            }
+
+            LEFT->{
+                binding.myTank.rotation = 270f
+                if(binding.myTank.marginLeft>0){
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= CELL_SIZE
+                }
+            }
+
+            RIGHT->{
+                binding.myTank.rotation = 90f
+                if(binding.myTank.marginLeft+ binding.myTank.width< binding.container.width/ CELL_SIZE* CELL_SIZE){
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= CELL_SIZE
+                }
+            }
         }
-        DOWN -> {
-            binding.myTank.rotation = 180f
-            (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
-        }
-        LEFT -> {
-            binding.myTank.rotation = 270f
-            (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
-        }
-        DOWN -> {
-            binding.myTank.rotation = 180f
-            (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
-        }
-    }
         binding.container.removeView(binding.myTank)
-                binding . container . addView (binding.myTank)
+        binding.container.addView(binding.myTank)
     }
 }
