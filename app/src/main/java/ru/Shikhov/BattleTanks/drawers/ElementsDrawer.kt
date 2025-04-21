d5bd1aca10a0d11feeb2c005f55fcd74fbbf5af1
ALT05122 <radmir.shihov@yandex.ru>
2025-04-21 22:30:15 +0300

    5 presentation

--- app/src/main/java/ru/Shikhov/BattleTanks/drawers/ElementsDrawer.kt
+++ app/src/main/java/ru/Shikhov/BattleTanks/drawers/ElementsDrawer.kt
@@ -3,8 +3,12 @@ package ru.Shikhov.BattleTanks.drawers
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
 import ru.Shikhov.BattleTanks.CELL_SIZE
 import ru.Shikhov.BattleTanks.R
+import ru.Shikhov.BattleTanks.binding
+import ru.Shikhov.BattleTanks.enums.Direction
 import ru.Shikhov.BattleTanks.enums.Material
 import ru.Shikhov.BattleTanks.models.Coordinate
 import ru.Shikhov.BattleTanks.models.Element
@@ -38,5 +42,87 @@ class ElementsDrawer(val container: FrameLayout) {
         view.layoutParams = layoutParams
         container.addView(view)
         elementsOnContainer.add(Element(viewId,currentMaterial,coordinate))
+    }
+    fun move(myTank: View,direction: Direction){
+        val layoutParams = myTank.layoutParams as FrameLayout.LayoutParams
+        val currentCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        when (direction) {
+            Direction.UP ->{
+                myTank.rotation = 0f
+                    (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+
+            }
+
+            Direction.DOWN -> {
+
+                    myTank.rotation = 180f
+                    (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+
+            }
+
+            Direction.LEFT -> {
+                    myTank.rotation = 270f
+                    (myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= CELL_SIZE
+                }
+
+            Direction.RIGHT -> {
+                    myTank.rotation = 90f
+                    (myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= CELL_SIZE
+
+                }
+            }
+
+                val nextCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+            if (checkTankCanMoveThroughBorder(
+                        nextCoordinate,
+            myTank
+                ) && checkTankCanMoveThroughMaterial(nextCoordinate)
+                ) {
+
+                binding.container.removeView(myTank)
+                binding.container.addView(myTank)
+            } else {
+            (myTank.layoutParams as FrameLayout.LayoutParams).topMargin = currentCoordinate.top
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin = currentCoordinate.left
+        }
+    }
+
+
+    private fun getElementByCoordinates (coordinate: Coordinate) =
+        elementsOnContainer.firstOrNull {it.coordinate == coordinate }
+
+    private fun checkTankCanMoveThroughMaterial (coordinate: Coordinate): Boolean {
+        getTankCoordinates(coordinate).forEach {
+            val element = getElementByCoordinates(it)
+            if (element != null && !element.material.tankConGoThrough) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkTankCanMoveThroughBorder(coordinate: Coordinate, myTank: View): Boolean {
+        if (coordinate.top >= 0 &&
+            coordinate.top + myTank.height <= binding.container.height  &&
+            coordinate.left >= 0 &&
+            coordinate.left + myTank.width <= binding.container.width
+            ) {
+            return true
+        }
+        return false
+    }
+
+    private fun getTankCoordinates(topLeftCoordinate: Coordinate): List<Coordinate> {
+        val coordinateList = mutableListOf<Coordinate>()
+        coordinateList.add(topLeftCoordinate)
+        coordinateList.add(Coordinate(topLeftCoordinate.top + CELL_SIZE, topLeftCoordinate.left))
+        coordinateList.add(Coordinate(topLeftCoordinate.top, topLeftCoordinate.left + CELL_SIZE))
+        coordinateList.add(
+            Coordinate(
+                topLeftCoordinate.top + CELL_SIZE,
+                topLeftCoordinate.left + CELL_SIZE
+            )
+        )
+        return coordinateList
     }
 }
--- app/src/main/java/ru/Shikhov/BattleTanks/models/Element.kt
+++ app/src/main/java/ru/Shikhov/BattleTanks/models/Element.kt
@@ -3,8 +3,7 @@ package ru.Shikhov.BattleTanks.models
 import ru.Shikhov.BattleTanks.enums.Material
 data class Element(
     val veiwId:Int,
-    val materials: Material,
+    val material: Material,
     val coordinate: Coordinate
 ){
-
 }
