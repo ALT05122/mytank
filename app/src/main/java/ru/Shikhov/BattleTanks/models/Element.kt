13c9e26a1702831742e3aa0e2e6de208933219a3
ALT05122 <radmir.shihov@yandex.ru>
2025-05-20 00:15:19 +0300

    15 presentation

--- app/src/main/java/ru/Shikhov/BattleTanks/models/Element.kt
+++ app/src/main/java/ru/Shikhov/BattleTanks/models/Element.kt
@@ -3,7 +3,7 @@ package ru.Shikhov.BattleTanks.models
 import android.view.View
 import ru.Shikhov.BattleTanks.enums.Material
 data class Element constructor(
-    val veiwId: Int = View.generateViewId(),
+    val viewId: Int = View.generateViewId(),
     val material: Material,
     val coordinate: Coordinate,
     val width: Int,
