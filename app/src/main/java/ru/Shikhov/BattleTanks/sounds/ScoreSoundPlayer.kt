da88b611df9360b970e0f75cae885616a9093c0d
ALT05122 <radmir.shihov@yandex.ru>
2025-05-30 22:17:31 +0300

    30 presentation

---
+++ app/src/main/java/ru/Shikhov/BattleTanks/sounds/ScoreSoundPlayer.kt
@@ -0,0 +1,35 @@
+package ru.Shikhov.BattleTanks.sounds
+
+import android.content.Context
+import android.content.pm.PackageManager.OnChecksumsReadyListener
+import ru.Shikhov.BattleTanks.R
+
+class ScoreSoundPlayer(
+    private val context: Context,
+    private val soundReadyListener: () -> Unit
+) {
+    private lateinit var scoreSound: GameSound
+    private val soundPool = SoundPoolFactory().createSoundPool()
+
+    init {
+        loadSounds()
+    }
+
+    private fun loadSounds(){
+        scoreSound = GameSound(
+            resourceInPool = soundPool.load(context, R.raw.score_count,1),
+            pool = soundPool
+        )
+    }
+
+    fun playScoreSound(){
+        soundPool.setOnLoadCompleteListener{ _, _, _ ->
+            soundReadyListener()
+            scoreSound.startOrResume(isLooping = true)
+        }
+    }
+
+    fun pauseScoreSound(){
+        scoreSound.pause()
+    }
+}
